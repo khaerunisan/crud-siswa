ef81daf660f85f4811e86db3979863413c15f3c6
khaerunisan <khaerunisan724@gmail.com>
2025-07-31 02:17:54 -0700

    sudah membuat index dan create di web dan views

---
+++ crud-siswa/resources/views/Siswa/index.blade.php
@@ -0,0 +1,39 @@
+<DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Baru</title>
+</head>
+    <body>
+
+        <h1>Halaman Beranda</h1>
+        <h1>List Data Siswa</h1>
+        <a href="Siswa/create">Tambah</a>
+        <table border="1" cellpadding="10" cellspacing="0">
+                <thead>
+                    <tr>
+                    <th>Foto</th>
+                    <th>Name</th>
+                    <th>Alamat</th>
+                    <th>Kelas</th>
+                    <th>Option</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Foto.jpg</td>
+                    <td>Nisa</td>
+                    <td>XI PPLG 3</td>
+                    <td>Jl ABCD</td>
+                    <td class="option-links">
+                        <a href="#">Hapus</a>
+                        <a href="#">Edit</a>
+                        <a href="#">Detail</a>
+                    </td>
+                </tr>
+            <tbody>
+        </table>
+    </div>
+    </body>
+    </html>
