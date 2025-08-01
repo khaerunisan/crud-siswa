c0cfaa55d16154a8086907dd9de650beee25788d
khaerunisan <khaerunisan724@gmail.com>
2025-08-01 01:54:20 -0700

    sudah mengerjakan siswa controller

--- crud-siswa/resources/views/Siswa/create.blade.php
+++ crud-siswa/resources/views/Siswa/create.blade.php
@@ -9,62 +9,67 @@
             <h1>Halaman Tambah Siswa</h1>
             <h1> Tambah Data Siswa</h1>
             <a href="/">Kembali</a>
-
-            <from action="" method="post">
-    </body>
-    <br>
-    <div>
-    <br>
-        <label for="">Kelas</label>
-        <select name="kelas">
-            <option value="">XII PPLG 1</option>
-            <option value="">XII PPLG 2</option>
-            <option value="">XII PPLG 3</option>
-    </select>
-    </div>
+            <form action="/Siswa/store"method="post"> 
+                @csrf
+            <br>
+            <div>
+            <br>
+            <label for="">Kelas</label>
+            <select name="kelas">
+                <option value="1">XII PPLG 1</option>
+                <option value="2">XII PPLG 2</option>
+                <option value="3">XII PPLG 3</option>
+            </select>
+             </div>
 </br>
 
     <div>
         <label for="">Name:</label>
         <br>
         <input type="text" name="name">
-</div>
+    </div>
+    <br>
+            <div>
+                <label for="">Nisn:</label>
+                <br>
+                <input type="text" name="nisn">
+        </div>
+    <br>
+        <div>
+                <label for="">Alamat:</label>
+                <br>
+                <input type="text" name="alamat">
+        </div>
+    <br>
+        <div>
+                <label for="">Email:</label>
+                <br>
+                <input type="text" name="email">
+        </div>
+    <br>
+            <div>
+                    <label for="">Password:</label>
+                    <br>
+                    <input type="password" name="password">
+            </div>
+        <br>
+                <div>
+                    <label for="">No Handphone:</label>
+                    <br>
+                    <input type="text" name="no_handphone">
+                </div>
+        <br>
+                <div>
+                    <label for="">Foto:</label>
+                    <br>
+                    <input type="file" name="foto">
+                </div>
 <br>
         <div>
-            <label for="">Nisn:</label>
-            <br>
-            <input type="text" name="nisn">
-    </div>
-<br>
-    <div>
-            <label for="">Alamat:</label>
-            <br>
-            <input type="text" name="alamat">
-    </div>
-<br>
-    <div>
-            <label for="">Email:</label>
-            <br>
-            <input type="text" name="email">
-    </div>
-<br>
-    <div>
-            <label for="">Password:</label>
-            <br>
-            <input type="password" name="password">
-    </div>
-<br>
-        <div>
-            <label for="">No Handphone:</label>
-            <br>
-            <input type="text" name="no_handphone">
-    </div>
-<br>
-        <div>
-            <label for="">Foto:</label>
-            <br>
-            <input type="file" name="foto">
-    </div>
-<br>
- <button type="submit">Simpan</button>
-</html>
+        <button type="submit">Simpan</button>
+        </div>
+        </form>
+    </body>
+            </form>
+    </body>
+        </html>
